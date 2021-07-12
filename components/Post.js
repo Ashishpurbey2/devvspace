848826d72f97957e567a5e49be8dfec4943dfd40
Ashish Kumar <ashishpurbey221806@gmail.com>
2021-07-12 17:52:28 +0530

    commit

--- components/Post.js
+++ components/Post.js
@@ -38,7 +38,7 @@ export default function Post({ post, compact }) {
             <a className='text-gray-900 hover:text-blue-600'>Read More</a>
           </Link>
           <div className='flex items-center'>
-            <img
+            <Image
               src={post.frontmatter.author_image}
               alt=''
               className='mx-4 w-10 h-10 object-cover rounded-full hidden sm:block'
